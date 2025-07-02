97e17af77deeb4f59a0d13da54ff9cdf889e3f50
Mashudu <62095778+MashuduWW@users.noreply.github.com>
2025-07-02 12:33:26 +0200

    Defined Aggregations and Window Functions.

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,19 @@
+-- Total number of bookings made by each user
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM users u
+LEFT JOIN bookings b ON u.user_id = b.guest_id
+GROUP BY u.user_id, u.first_name, u.last_name;
+
+-- Rank properties based on the total number of bookings (most to least)
+SELECT 
+    p.property_id,
+    p.title,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM properties p
+LEFT JOIN bookings b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.title;
